fe9ac38a973dd52368251fb67027541d0f0c1251
twirtgen <thomas.wirtgen@uclouvain.be>
2020-11-27 18:33:40 +0100

    Fixing compilation issues with the new version of the library

--- sysdep/unix/main.c
+++ sysdep/unix/main.c
@@ -46,42 +46,7 @@
 #include "unix.h"
 #include "krt.h"
 
-
-static proto_ext_fun_t funcs[] = {
-        {.fn = add_attr, .name="add_attr"},
-        {.fn = set_attr, .name="set_attr"},
-        {.fn = get_attr, .name="get_attr"},
-        {.fn = write_to_buffer, .name="write_to_buffer"},
-        {.fn = get_attr_by_code_from_rte, .name = "get_attr_by_code_from_rte"},
-        {.fn = get_peer_info, .name = "get_peer_info"},
-        {.fn = get_src_peer_info, .name = "get_src_peer_info"},
-        {.fn = set_peer_info_src, .name = "set_peer_info_src"},
-        {.fn = set_peer_info, .name = "set_peer_info"},
-        {.fn = get_peer_info_src_extra, .name = "get_peer_info_src_extra"},
-        {.fn = get_peer_info_extra, .name = "get_peer_info_src_extra"},
-        {.fn = get_attr_from_code, .name = "get_attr_from_code"},
-        {.fn = get_prefix, .name = "get_prefix"},
-        proto_ext_func_null,
-};
-
-static insertion_point_info_t plugins[] = {
-        {.insertion_point_id = BGP_PRE_DECISION, .insertion_point_str="bgp_pre_decision"},
-        {.insertion_point_id = BGP_NEXTHOP_RESOLVABLE_DECISION, .insertion_point_str="bgp_nexthop_resolvable_decision"},
-        {.insertion_point_id = BGP_LOCAL_PREF_DECISION, .insertion_point_str="bgp_local_pref_decision"},
-        {.insertion_point_id = BGP_AS_PATH_LENGTH_DECISION, .insertion_point_str="bgp_as_path_length_decision"},
-        {.insertion_point_id = BGP_MED_DECISION, .insertion_point_str="bgp_med_decision"},
-        {.insertion_point_id = BGP_USE_ORIGIN_DECISION, .insertion_point_str="bgp_use_origin_decision"},
-        {.insertion_point_id = BGP_PREFER_EXTERNAL_PEER_DECISION, .insertion_point_str="bgp_prefer_external_peer_decision"},
-        {.insertion_point_id = BGP_IGP_COST_DECISION, .insertion_point_str="bgp_igp_cost_decision"},
-        {.insertion_point_id = BGP_ROUTER_ID_DECISION, .insertion_point_str="bgp_router_id_decision"},
-        {.insertion_point_id = BGP_IPADDR_DECISION, .insertion_point_str="bgp_ipaddr_decision"},
-        {.insertion_point_id = BGP_POST_DECISION, .insertion_point_str="bgp_post_decision"},
-        {.insertion_point_id = BGP_DECODE_ATTR, .insertion_point_str="bgp_decode_attr"},
-        {.insertion_point_id = BGP_ENCODE_ATTR, .insertion_point_str="bgp_encode_attr"},
-        {.insertion_point_id = BGP_PRE_INBOUND_FILTER, .insertion_point_str="bgp_pre_inbound_filter"},
-        {.insertion_point_id = BGP_PRE_OUTBOUND_FILTER, .insertion_point_str="bgp_pre_outbound_filter"},
-        insertion_point_info_null
-};
+#include <xbgp_compliant_api/xbgp_api_vars.h>
 
 static const char *plugin_manifest = NULL;
 static const char *plugin_extra_conf = NULL;
@@ -998,11 +963,11 @@ main(int argc, char **argv)
        the_plugin_extra_conf = PATH_CONFIG_PLUGINS_EXTRA_CONF;
    }
 
-  if (init_plugin_manager(funcs, PATH_CONFIG_PLUGINS_DIR, strnlen(PATH_CONFIG_PLUGINS_DIR, 2048),
-                          plugins, NULL, NULL, 0) != 0)
+  if (init_plugin_manager(api_funcs, PATH_CONFIG_PLUGINS_DIR, strnlen(PATH_CONFIG_PLUGINS_DIR, 2048),
+                          insertion_points, NULL, NULL, 0) != 0)
       die("Cannot init plugin manager");
 
-  if(load_extension_code(the_plugin_manifest, the_plugin_location_dir, funcs, plugins) != 0) {
+  if(load_extension_code(the_plugin_manifest, the_plugin_location_dir, api_funcs, insertion_points) != 0) {
       die("Unable to load plugins");
   }
 
